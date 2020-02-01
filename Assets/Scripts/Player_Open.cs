e33c9d6e230d8e99bb8b24997b4bfbd5ab4cff4f
tihoson <alvi99@inbox.ru>
2020-02-01 20:05:09 +0200

    add pills

--- Assets/Scripts/Player_Open.cs
+++ Assets/Scripts/Player_Open.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 [RequireComponent(typeof(Camera))]
 
 public class Player_Open : MonoBehaviour
 {
-
-    public float distance = 5; // в приделах этой дистанции дверь будет доступна
+    int pillsCount = 0;
+    int maxPillsCount = 3;
+    public float doorDistance = 3; // в приделах этой дистанции дверь будет доступна
+    public float pillDistance = 3; // в приделах этой дистанции дверь будет доступна
     public string doorTag = "Door"; // тег двери
+    public string pillTag = "Pill"; // тег колёс
     public KeyCode key = KeyCode.F; // клавиша управления
+    private float time;
     private Camera cam;
 
+    void Start() 
+    {
+        time = Time.time;
+    }
     void Awake()
     {
         cam = GetComponent<Camera>();
@@ -22,12 +31,24 @@ public class Player_Open : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-            if (Physics.Raycast(ray, out hit, distance))
+            if (Physics.Raycast(ray, out hit, doorDistance, ~(1<<9)))
             {
                 if (hit.collider.tag == doorTag)
                 {
                     hit.transform.GetComponent<Open_Close_Controller>().enabled = true;
                     hit.transform.GetComponent<Open_Close_Controller>().Invert(transform);
+                }
+            }
+            if (Physics.Raycast(ray, out hit, pillDistance, ~(1<<9)))
+            {
+                if (hit.collider.tag == pillTag)
+                {
+                    Destroy(hit.transform.gameObject);
+                    pillsCount++;
+                    if(pillsCount >= maxPillsCount){
+                        SceneManager.LoadScene("FinalMenu");
+                        GlobalParams.record = Time.time - time;
+                    }
                 }
             }
         }

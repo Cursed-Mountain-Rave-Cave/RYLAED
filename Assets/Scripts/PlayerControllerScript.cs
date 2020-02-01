c97d73131efd58d053f3f522518176c0fd8c4dec
tihoson <alvi99@inbox.ru>
2020-02-01 17:39:11 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerControllerScript : MonoBehaviour
+{ 
+    private CharacterController charController;
+    private int cameraSpeed = 270;
+    private float playerSpeed = 3f;
+    private float squatScale = 0.5f;
+    private float gravity = -9.81f;
+    private Camera not_camera;
+    private Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        not_camera = GetComponentInChildren<Camera>();
+        charController = GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {   
+        HandleLook();
+        HandleMove();
+        HandleSprint();
+        HandleSquat();
+    }
+
+    private void HandleLook()
+    {
+
+        
+        if (Math.Abs(not_camera.transform.rotation.eulerAngles.x - Time.deltaTime * cameraSpeed * Input.GetAxis("Mouse Y") - 180)  > 90) 
+        {
+            not_camera.transform.Rotate(-Time.deltaTime * cameraSpeed * Input.GetAxis("Mouse Y"), 0, 0);
+        }
+        transform.Rotate(0, Time.deltaTime * cameraSpeed * Input.GetAxis("Mouse X"), 0);
+    }
+    private void HandleMove()
+    {       
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        charController.Move(move * playerSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        charController.Move(velocity * Time.deltaTime);
+
+    }
+    private void HandleSquat()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.localScale -= Vector3.up * squatScale;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.localScale += Vector3.up * squatScale;
+        }
+    }
+    private void HandleSprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            playerSpeed *= 2;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+           playerSpeed /= 2;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TextTimerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Mathf;
+using UnityEngine.UI;
+
+public class TextTimerScript : MonoBehaviour
+{
+    public Text timerLabel;
+    private float time;
+    private int fontSize;
+    private float deadline = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fontSize = timerLabel.fontSize;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        time += Time.deltaTime;
+
+        var minutes = time / 60;
+        var seconds = time % 60;
+        var fraction = (time * 100) % 100;
+
+        //update the label value
+        if (seconds >= 20)
+            timerLabel.text = "Ты пидорас";
+        else
+            timerLabel.text = string.Format ("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+
+        if (seconds > deadline && seconds < 20) 
+        {
+            timerLabel.color = new Color(1, 0, 0, Abs(Sin(Pow(time, 3) / 6)));
+            timerLabel.fontSize = fontSize + (int)Max(0, seconds - deadline); 
+        }
+    }
+}

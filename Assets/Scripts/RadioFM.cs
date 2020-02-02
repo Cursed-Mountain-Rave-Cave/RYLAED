aafad9d0cf35798cca6e13e3e06f661a03720b2a
EZavdovev <45925246+EZavdovev@users.noreply.github.com>
2020-02-02 14:01:58 +0200

    radioFM
    more music from genius

--- Assets/Scripts/RadioFM.cs
+++ Assets/Scripts/RadioFM.cs
@@ -8,11 +8,19 @@ public class RadioFM : MonoBehaviour
     AudioClip[] sounds;
     public AudioClip one;
     public AudioClip two;
+    public AudioClip three;
+    public AudioClip four;
+    public AudioClip five;
+    public AudioClip six;
     void Start()
     {
-        sounds = new AudioClip[2];
+        sounds = new AudioClip[6];
         sounds[0] = one;
         sounds[1] = two;
+        sounds[2] = three;
+        sounds[3] = four;
+        sounds[4] = five;
+        sounds[5] = six;
         RadioMusic = GetComponent<AudioSource>();
         RadioMusic.clip = sounds[Random.Range(0, sounds.Length)];
         RadioMusic.Play();

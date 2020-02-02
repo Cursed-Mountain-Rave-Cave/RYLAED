40f102803443b5c4924e844d6d13270fe5e01987
Михаил Добрилко <dobmisha@mail.ru>
2020-02-02 11:32:17 +0200

    added paintings and loaded sounds

---
+++ Assets/Scripts/RadioFM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioFM : MonoBehaviour
+{
+    AudioSource RadioMusic;
+    AudioClip[] sounds;
+    public AudioClip one;
+    public AudioClip two;
+    void Start()
+    {
+        sounds = new AudioClip[2];
+        sounds[0] = one;
+        sounds[1] = two;
+        RadioMusic = GetComponent<AudioSource>();
+        RadioMusic.clip = sounds[Random.Range(0, sounds.Length)];
+        RadioMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TextTimerScript.cs
+++ Assets/Scripts/TextTimerScript.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.Mathf;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TextTimerScript : MonoBehaviour
 {
     public Text timerLabel;
     private float time;
     private int fontSize;
-    private float deadline = 5;
+    private float deadline = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +26,12 @@ public class TextTimerScript : MonoBehaviour
         var fraction = (time * 100) % 100;
 
         //update the label value
-        if (seconds >= 20)
-            timerLabel.text = "Ты пидорас";
+        if (time >= 20)
+            SceneManager.LoadScene("DedMenu");
         else
-            timerLabel.text = string.Format ("{0:00} : {1:00} : {2:000}", minutes, seconds, fraction);
+            timerLabel.text = string.Format ("{0:00} : {1:00} : {2:000}", minutes, 20 - seconds, 100 - fraction);
 
-        if (seconds > deadline && seconds < 20) 
+        if (time > deadline && time < 20) 
         {
             timerLabel.color = new Color(1, 0, 0, Abs(Sin(Pow(time, 3) / 6)));
             timerLabel.fontSize = fontSize + (int)Max(0, seconds - deadline); 
